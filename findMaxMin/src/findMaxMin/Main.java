0d4ed7a8260e3d63bcbb09ba6e5c64a035b90335
Burcubayik <92742235+Burcubayik@users.noreply.github.com>
2022-02-18 13:36:36 +0300

    added findMaxMin

---
+++ findMaxMin/src/findMaxMin/Main.java
@@ -0,0 +1,30 @@
+package findMaxMin;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int max=0, min=0;
+		Scanner input=new Scanner(System.in);
+		int n,number;
+		System.out.print("Kaç sayý gireceksiniz? ");
+		n=input.nextInt();
+		
+		for(int i=1; i<=n;i++) {
+			System.out.print(i+ ". Sayýyý giriniz: ");
+			number=input.nextInt();
+			if(max==0&&min==0) {
+				max=number;
+				min=number;
+			}
+			
+			if(max<number) max=number;
+			if(min>number) min=number;
+		}
+		 System.out.println("En büyük sayý: "+ max);
+		 System.out.println("En küçük sayý: "+min);
+
+	}
+
+}
